c5f05bf467205310b37c889c7f4fa7c3416849e9
Yuu Shimizu <p@yuushimizu.com>
2022-04-03 15:06:18 +0900

    add code to destination

--- src/rom/header/destination.rs
+++ src/rom/header/destination.rs
@@ -1,14 +1,30 @@
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub enum Destination {
+pub enum DestinationType {
     Japanese,
     NonJapanese,
 }
 
+impl From<u8> for DestinationType {
+    fn from(code: u8) -> Self {
+        use DestinationType::*;
+        match code {
+            0x00 => Japanese,
+            _ => NonJapanese,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Destination {
+    code: u8,
+    destination_type: DestinationType,
+}
+
 impl From<u8> for Destination {
-    fn from(value: u8) -> Self {
-        match value {
-            0x01 => Self::NonJapanese,
-            _ => Self::Japanese,
+    fn from(code: u8) -> Self {
+        Self {
+            code,
+            destination_type: code.into(),
         }
     }
 }
