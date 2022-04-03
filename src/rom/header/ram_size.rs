96f2dded04cbd02ad7ce89f842b8ce7f29265502
Yuu Shimizu <p@yuushimizu.com>
2022-04-03 15:11:47 +0900

    revert ram size enum values

--- src/rom/header/ram_size.rs
+++ src/rom/header/ram_size.rs
@@ -1,18 +1,22 @@
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum RamSizeAmount {
     Unknown,
-    Kb(i64),
+    None,
+    Kb8,
+    Kb32,
+    Kb128,
+    Kb64,
 }
 
 impl From<u8> for RamSizeAmount {
     fn from(code: u8) -> Self {
         use RamSizeAmount::*;
         match code {
-            0x00 => Kb(0),
-            0x02 => Kb(8),
-            0x03 => Kb(32),
-            0x04 => Kb(128),
-            0x05 => Kb(64),
+            0x00 => None,
+            0x02 => Kb8,
+            0x03 => Kb32,
+            0x04 => Kb128,
+            0x05 => Kb64,
             _ => Unknown,
         }
     }
