b919ee5bf6d27ff7c9b32eac982f9043461ceb0a
Yuu Shimizu <p@yuushimizu.com>
2022-04-03 14:08:48 +0900

    add header mod

---
+++ src/rom/header/mod.rs
@@ -0,0 +1,45 @@
+mod cartridge_type;
+mod cgb_flag;
+mod destination;
+mod licensee;
+mod ram_size;
+mod rom_size;
+mod sgb_flag;
+mod title;
+
+pub use cartridge_type::{CartridgeType, MBCType};
+pub use cgb_flag::CGBFlag;
+pub use destination::Destination;
+pub use licensee::Licensee;
+pub use ram_size::RamSize;
+pub use rom_size::RomSize;
+pub use sgb_flag::SGBFlag;
+
+#[derive(Debug)]
+pub struct Header {
+    pub entry_point: [u8; 4],
+    pub cgb_flag: CGBFlag,
+    pub title: String,
+    pub licensee: Licensee,
+    pub sgb_flag: SGBFlag,
+    pub cartridge_type: CartridgeType,
+    pub rom_size: RomSize,
+    pub ram_size: RamSize,
+    pub destination: Destination,
+}
+
+impl Header {
+    pub fn load(bytes: &[u8]) -> Self {
+        Self {
+            entry_point: bytes[0x0100..=0x0103].try_into().unwrap(),
+            cgb_flag: CGBFlag::load(bytes),
+            title: title::load(bytes),
+            licensee: licensee::Licensee::load(bytes),
+            sgb_flag: SGBFlag::load(bytes),
+            cartridge_type: CartridgeType::load(bytes),
+            rom_size: RomSize::load(bytes),
+            ram_size: RamSize::load(bytes),
+            destination: Destination::load(bytes),
+        }
+    }
+}
--- src/rom/mod.rs
+++ src/rom/mod.rs
@@ -1,45 +1,16 @@
-mod cartridge_type;
-mod cgb_flag;
-mod destination;
-mod licensee;
-mod ram_size;
-mod rom_size;
-mod sgb_flag;
-mod title;
+pub mod header;
 
-pub use cartridge_type::{CartridgeType, MBCType};
-pub use cgb_flag::CGBFlag;
-pub use destination::Destination;
-pub use licensee::Licensee;
-pub use ram_size::RamSize;
-pub use rom_size::RomSize;
-pub use sgb_flag::SGBFlag;
+pub use header::Header;
 
 #[derive(Debug)]
 pub struct Rom {
-    pub entry_point: [u8; 4],
-    pub cgb_flag: CGBFlag,
-    pub title: String,
-    pub licensee: Licensee,
-    pub sgb_flag: SGBFlag,
-    pub cartridge_type: CartridgeType,
-    pub rom_size: RomSize,
-    pub ram_size: RamSize,
-    pub destination: Destination,
+    pub header: Header,
 }
 
 impl Rom {
     pub fn load(bytes: &[u8]) -> Self {
         Self {
-            entry_point: bytes[0x0100..=0x0103].try_into().unwrap(),
-            cgb_flag: CGBFlag::load(bytes),
-            title: title::load(bytes),
-            licensee: licensee::Licensee::load(bytes),
-            sgb_flag: SGBFlag::load(bytes),
-            cartridge_type: CartridgeType::load(bytes),
-            rom_size: RomSize::load(bytes),
-            ram_size: RamSize::load(bytes),
-            destination: Destination::load(bytes),
+            header: Header::load(bytes),
         }
     }
 }
