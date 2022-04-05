df8df29b7476d61a03cb1c7241a2605f69a2c327
Yuu Shimizu <p@yuushimizu.com>
2022-04-05 19:31:28 +0900

    fix!

--- core/src/cpu/command/mod.rs
+++ core/src/cpu/command/mod.rs
@@ -13,7 +13,7 @@ pub struct Command {
 
 impl fmt::Debug for Command {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_struct("U8Destination")
+        f.debug_struct("Command")
             .field("name", &self.opcode)
             .field("mnemonic", &self.mnemonic)
             .field("cycles", &self.cycles)
