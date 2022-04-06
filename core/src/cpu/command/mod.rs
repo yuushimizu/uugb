ec3bd7e3a0230120ff1cc3771c3187f87a1dc68a
Yuu Shimizu <p@yuushimizu.com>
2022-04-06 14:55:10 +0900

    implement conditional jumps

--- core/src/cpu/command/mod.rs
+++ core/src/cpu/command/mod.rs
@@ -245,6 +245,10 @@ impl Command {
             0x1F => (rra(), 4),
             // Jumps
             0xC3 => (jp(LITERAL), 12),
+            0xC2 => (jp_nz(LITERAL), 12),
+            0xCA => (jp_z(LITERAL), 12),
+            0xD2 => (jp_nc(LITERAL), 12),
+            0xDA => (jp_c(LITERAL), 12),
             // Not Implemented
             _ => panic!("This opcode is not implemented!: {:02X}", opcode),
         };
--- core/src/cpu/command/operator/jump.rs
+++ core/src/cpu/command/operator/jump.rs
@@ -1,8 +1,33 @@
 use super::Operator;
-use crate::cpu::command::operand::ReadRef;
+use crate::cpu::{command::operand::ReadRef, registers::Flags};
 
-pub fn jp(location: ReadRef<u16>) -> Operator {
+pub fn jp(operand: ReadRef<u16>) -> Operator {
     Operator::new("JP", |context| {
-        context.registers_mut().pc = location.read(context);
+        context.registers_mut().pc = operand.read(context);
     })
+}
+
+fn conditional_jump(operand: ReadRef<u16>, condition: fn(Flags) -> bool) -> Operator {
+    Operator::new("JP", move |context| {
+        let address = operand.read(context);
+        if condition(context.flags()) {
+            context.registers_mut().pc = address;
+        }
+    })
+}
+
+pub fn jp_nz(operand: ReadRef<u16>) -> Operator {
+    conditional_jump(operand, |flags| !flags.z)
+}
+
+pub fn jp_z(operand: ReadRef<u16>) -> Operator {
+    conditional_jump(operand, |flags| flags.z)
+}
+
+pub fn jp_nc(operand: ReadRef<u16>) -> Operator {
+    conditional_jump(operand, |flags| !flags.c)
+}
+
+pub fn jp_c(operand: ReadRef<u16>) -> Operator {
+    conditional_jump(operand, |flags| flags.c)
 }
