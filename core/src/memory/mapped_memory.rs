06d6e1dc6d20ea99bb297d7c9b74e7d3ce2106f4
Yuu Shimizu <p@yuushimizu.com>
2022-04-07 20:31:23 +0900

    refactor

--- core/src/memory/mapped_memory.rs
+++ core/src/memory/mapped_memory.rs
@@ -15,112 +15,112 @@ mod segment {
     use super::Components;
 
     pub struct Segment {
-        pub read: fn(&Components, u16) -> u8,
-        pub write: fn(&mut Components, u16, u8),
+        pub reader: Box<dyn Fn(&Components, u16) -> u8>,
+        pub writer: Box<dyn Fn(&mut Components, u16, u8)>,
     }
 
     impl Segment {
+        pub fn new<
+            R: Fn(&Components, u16) -> u8 + 'static,
+            W: Fn(&mut Components, u16, u8) + 'static,
+        >(
+            reader: R,
+            writer: W,
+        ) -> Self {
+            Self {
+                reader: Box::new(reader),
+                writer: Box::new(writer),
+            }
+        }
+
         pub fn read(&self, components: &Components, address: u16) -> u8 {
-            (self.read)(components, address)
+            (self.reader)(components, address)
         }
 
         pub fn write(&self, components: &mut Components, address: u16, value: u8) {
-            (self.write)(components, address, value);
+            (self.writer)(components, address, value);
         }
     }
 
-    pub const CARTRIDGE: Segment = Segment {
-        read: |components, address| components.cartridge.read(address),
-        write: |components, address, value| components.cartridge.write(address, value),
-    };
+    pub fn cartridge() -> Segment {
+        Segment::new(
+            |components, address| components.cartridge.read(address),
+            |components, address, value| components.cartridge.write(address, value),
+        )
+    }
 
-    pub const WRAM: Segment = {
-        const BASE_ADDRESS: u16 = 0xC000;
-        Segment {
-            read: |components, address| components.wram.read(address - BASE_ADDRESS),
-            write: |components, address, value| {
-                components.wram.write(address - BASE_ADDRESS, value)
+    pub fn wram(base_address: u16) -> Segment {
+        Segment::new(
+            move |components, address| components.wram.read(address - base_address),
+            move |components, address, value| components.wram.write(address - base_address, value),
+        )
+    }
+
+    pub fn unusable() -> Segment {
+        Segment::new(|_, _| 0xFF, |_, _, _| {})
+    }
+
+    pub fn hram(base_address: u16) -> Segment {
+        Segment::new(
+            move |components, address| components.hram.read(address - base_address),
+            move |components, address, value| {
+                components.hram.write(address - base_address, value);
             },
-        }
-    };
+        )
+    }
 
-    pub const WRAM_MIRROR: Segment = {
-        const BASE_ADDRESS: u16 = 0xE000;
-        Segment {
-            read: |components, address| components.wram.read(address - BASE_ADDRESS),
-            write: |components, address, value| {
-                components.wram.write(address - BASE_ADDRESS, value)
+    pub fn joypad() -> Segment {
+        Segment::new(
+            |components, _| components.joypad.bits(),
+            |components, _, value| {
+                components
+                    .joypad
+                    .set_bits(value, components.interrupt_controller)
             },
-        }
-    };
+        )
+    }
 
-    pub const UNUSABLE: Segment = Segment {
-        read: |_, _| 0xFF,
-        write: |_, _, _| {},
-    };
-
-    pub const HRAM: Segment = {
-        const BASE_ADDRESS: u16 = 0xFF80;
-        Segment {
-            read: |components, address| components.hram.read(address - BASE_ADDRESS),
-            write: |components, address, value| {
-                components.hram.write(address - BASE_ADDRESS, value);
-            },
-        }
-    };
-
-    pub const JOYPAD: Segment = Segment {
-        read: |components, _| components.joypad.bits(),
-        write: |components, _, value| {
-            components
-                .joypad
-                .set_bits(value, components.interrupt_controller)
-        },
-    };
-
-    pub const TIMER: Segment = {
-        fn inner_segment(address: u16) -> Segment {
+    pub fn timer() -> Segment {
+        fn inner(address: u16) -> Segment {
             match address {
-                0xFF04 => Segment {
-                    read: |components, _| components.timer.divider_register(),
-                    write: |components, _, _| components.timer.reset_divider(),
-                },
-                0xFF05 => Segment {
-                    read: |components, _| components.timer.counter(),
-                    write: |components, _, value| components.timer.set_counter(value),
-                },
-                0xFF06 => Segment {
-                    read: |components, _| components.timer.modulo(),
-                    write: |components, _, value| components.timer.set_modulo(value),
-                },
-                0xFF07 => Segment {
-                    read: |components, _| components.timer.control_bits(),
-                    write: |components, _, value| components.timer.set_control_bits(value),
-                },
+                0xFF04 => Segment::new(
+                    |components, _| components.timer.divider_register(),
+                    |components, _, _| components.timer.reset_divider(),
+                ),
+                0xFF05 => Segment::new(
+                    |components, _| components.timer.counter(),
+                    |components, _, value| components.timer.set_counter(value),
+                ),
+                0xFF06 => Segment::new(
+                    |components, _| components.timer.modulo(),
+                    |components, _, value| components.timer.set_modulo(value),
+                ),
+                0xFF07 => Segment::new(
+                    |components, _| components.timer.control_bits(),
+                    |components, _, value| components.timer.set_control_bits(value),
+                ),
                 _ => unreachable!(),
             }
         }
-        Segment {
-            read: |components, address| inner_segment(address).read(components, address),
-            write: |components, address, value| {
-                inner_segment(address).write(components, address, value)
-            },
-        }
-    };
+        Segment::new(
+            |components, address| inner(address).read(components, address),
+            |components, address, value| inner(address).write(components, address, value),
+        )
+    }
 
-    pub const INTERRUPT_REQUESTED: Segment = Segment {
-        read: |components, _address| components.interrupt_controller.requested_bits(),
-        write: |components, _address, value| {
-            components.interrupt_controller.set_requested_bits(value)
-        },
-    };
+    pub fn interrupt_requested() -> Segment {
+        Segment::new(
+            |components, _address| components.interrupt_controller.requested_bits(),
+            |components, _address, value| components.interrupt_controller.set_requested_bits(value),
+        )
+    }
 
-    pub const INTERRUPT_ENABLED: Segment = Segment {
-        read: |components, _address| components.interrupt_controller.enabled_bits(),
-        write: |components, _address, value| {
-            components.interrupt_controller.set_enabled_bits(value)
-        },
-    };
+    pub fn interrupt_enabled() -> Segment {
+        Segment::new(
+            |components, _address| components.interrupt_controller.enabled_bits(),
+            |components, _address, value| components.interrupt_controller.set_enabled_bits(value),
+        )
+    }
 }
 
 use segment::Segment;
@@ -136,18 +136,18 @@ impl<'a> MappedMemory<'a> {
     fn segment(&self, address: u16) -> Segment {
         use segment::*;
         match address {
-            0x0000..=0x7FFF => CARTRIDGE,
+            0x0000..=0x7FFF => cartridge(),
             0x8000..=0x9FFF => panic!("VRAM"),
-            0xA000..=0xBFFF => CARTRIDGE,
-            0xC000..=0xDFFF => WRAM,
-            0xE000..=0xFDFF => WRAM_MIRROR,
+            0xA000..=0xBFFF => cartridge(),
+            0xC000..=0xDFFF => wram(0xC000),
+            0xE000..=0xFDFF => wram(0xE000), // mirror
             0xFE00..=0xFE9F => panic!("OAM"),
-            0xFEA0..=0xFEFF => UNUSABLE,
-            0xFF00 => JOYPAD,
-            0xFF04..=0xFF07 => TIMER,
-            0xFF0F => INTERRUPT_REQUESTED,
-            0xFF80..=0xFFFE => HRAM,
-            0xFFFF => INTERRUPT_ENABLED,
+            0xFEA0..=0xFEFF => unusable(),
+            0xFF00 => joypad(),
+            0xFF04..=0xFF07 => timer(),
+            0xFF0F => interrupt_requested(),
+            0xFF80..=0xFFFE => hram(0xFF80),
+            0xFFFF => interrupt_enabled(),
             _ => panic!("Read from the address: {:04X}", address),
         }
     }
