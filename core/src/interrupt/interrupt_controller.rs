d2b72cc11dcebb986cd3c3902e4e34c262021856
Yuu Shimizu <p@yuushimizu.com>
2022-04-07 21:06:03 +0900

    refactor

--- core/src/interrupt/interrupt_controller.rs
+++ core/src/interrupt/interrupt_controller.rs
@@ -1,28 +1,62 @@
-use super::{Interrupt, States};
+use super::Interrupt;
+use crate::util::bits::Bits;
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Default)]
+pub struct State {
+    is_enabled: bool,
+    is_requested: bool,
+}
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Default)]
 pub struct InterruptController {
-    states: States,
+    states: [State; Interrupt::ORDERED.len()],
+}
+
+impl Interrupt {
+    fn bit(&self) -> u8 {
+        Self::ORDERED.iter().position(|x| x == self).unwrap() as u8
+    }
 }
 
 impl InterruptController {
-    pub fn enabled_bits(&self) -> u8 {
-        self.states.enabled_bits()
-    }
-
-    pub fn set_enabled_bits(&mut self, bits: u8) {
-        self.states.set_enabled_bits(bits)
-    }
-
-    pub fn requested_bits(&self) -> u8 {
-        self.states.requested_bits()
-    }
-
-    pub fn set_requested_bits(&mut self, bits: u8) {
-        self.states.set_requested_bits(bits)
+    fn state_mut(&mut self, interrupt: Interrupt) -> &mut State {
+        &mut self.states[interrupt.bit() as usize]
     }
 
     pub fn request(&mut self, interrupt: Interrupt) {
-        self.states.state_mut(interrupt).is_requested = true;
+        self.state_mut(interrupt).is_requested = true;
+    }
+
+    pub fn clear(&mut self, interrupt: Interrupt) {
+        self.state_mut(interrupt).is_requested = false;
+    }
+
+    fn bits<F: Fn(&State) -> bool>(&self, f: F) -> u8 {
+        self.states
+            .iter()
+            .enumerate()
+            .fold(0x00, |acc, (bit, state)| acc | (f(state) as u8) << bit)
+    }
+
+    fn set_bits<F: Fn(&mut State, bool)>(&mut self, f: F, bits: u8) {
+        for (bit, state) in self.states.iter_mut().enumerate() {
+            f(state, bits.bit(bit as u32))
+        }
+    }
+
+    pub fn enabled_bits(&self) -> u8 {
+        self.bits(|state| state.is_enabled)
+    }
+
+    pub fn set_enabled_bits(&mut self, bits: u8) {
+        self.set_bits(|state, bit| state.is_enabled = bit, bits);
+    }
+
+    pub fn requested_bits(&self) -> u8 {
+        self.bits(|state| state.is_requested)
+    }
+
+    pub fn set_requested_bits(&mut self, bits: u8) {
+        self.set_bits(|state, bit| state.is_requested = bit, bits);
     }
 }
--- core/src/interrupt/mod.rs
+++ core/src/interrupt/mod.rs
@@ -2,8 +2,6 @@ pub mod interrupt_controller;
 
 pub use interrupt_controller::InterruptController;
 
-use crate::util::bits::Bits;
-
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub enum Interrupt {
     VBlank,
@@ -14,79 +12,19 @@ pub enum Interrupt {
 }
 
 impl Interrupt {
-    fn definition(&self) -> (u8, u8) {
+    const ORDERED: [Interrupt; 5] = {
         use Interrupt::*;
-        match self {
-            VBlank => (0, 0x40),
-            LcdStat => (1, 0x48),
-            Timer => (2, 0x50),
-            Serial => (3, 0x58),
-            Joypad => (4, 0x60),
-        }
-    }
-
-    pub fn bit(&self) -> u8 {
-        self.definition().0
-    }
+        [VBlank, LcdStat, Timer, Serial, Joypad]
+    };
 
     pub fn address(&self) -> u8 {
-        self.definition().1
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Hash, Default)]
-pub struct State {
-    is_enabled: bool,
-    is_requested: bool,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Hash, Default)]
-pub struct States {
-    states: [State; 5],
-}
-
-impl States {
-    pub fn new(enabled_bits: u8, requested_bits: u8) -> Self {
-        let mut states = States::default();
-        states.set_enabled_bits(enabled_bits);
-        states.set_requested_bits(requested_bits);
-        states
-    }
-
-    fn bits<F: Fn(&State) -> bool>(&self, f: F) -> u8 {
-        self.states
-            .iter()
-            .enumerate()
-            .fold(0x00, |acc, (bit, state)| acc | (f(state) as u8) << bit)
-    }
-
-    fn set_bits<F: Fn(&mut State, bool)>(&mut self, f: F, bits: u8) {
-        for (bit, state) in self.states.iter_mut().enumerate() {
-            f(state, bits.bit(bit as u32))
+        use Interrupt::*;
+        match self {
+            VBlank => 0x40,
+            LcdStat => 0x48,
+            Timer => 0x50,
+            Serial => 0x58,
+            Joypad => 0x60,
         }
-    }
-
-    pub fn enabled_bits(&self) -> u8 {
-        self.bits(|state| state.is_enabled)
-    }
-
-    pub fn set_enabled_bits(&mut self, bits: u8) {
-        self.set_bits(|state, bit| state.is_enabled = bit, bits);
-    }
-
-    pub fn requested_bits(&self) -> u8 {
-        self.bits(|state| state.is_requested)
-    }
-
-    pub fn set_requested_bits(&mut self, bits: u8) {
-        self.set_bits(|state, bit| state.is_requested = bit, bits);
-    }
-
-    pub fn state(&self, interrupt: Interrupt) -> &State {
-        &self.states[interrupt.bit() as usize]
-    }
-
-    pub fn state_mut(&mut self, interrupt: Interrupt) -> &mut State {
-        &mut self.states[interrupt.bit() as usize]
     }
 }
