75e82d3bb62e91ec332c0609cf51939cf306df2f
Yuu Shimizu <p@yuushimizu.com>
2022-04-11 04:46:10 +0900

    add rendering background

--- core/src/ppu/mod.rs
+++ core/src/ppu/mod.rs
@@ -42,8 +42,7 @@ pub struct Ppu {
     vram: Vram,
     control: Control,
     interrupt_source: InterruptSource,
-    current_y: u8,
-    current_x: u8,
+    current_position: Coordinate,
     y_compare: u8,
     background_palette: Palette,
     object_palette0: Palette,
@@ -56,7 +55,7 @@ pub struct Ppu {
 impl Ppu {
     fn mode(&self) -> Mode {
         use Mode::*;
-        if self.current_y >= HEIGHT {
+        if self.current_position.y >= HEIGHT {
             VBlank
         } else if self.cycles_in_line < OAM_SEARCH_CYCLES {
             OamSearch
@@ -67,14 +66,36 @@ impl Ppu {
         }
     }
 
+    fn background_pixel(&self, position: Coordinate) -> u8 {
+        if !self.control.background_and_window_enabled() {
+            0b00
+        } else {
+            self.vram
+                .tile_map(
+                    self.control.background_tile_map_area(),
+                    self.control.background_tile_data_area(),
+                )
+                .pixel(position)
+        }
+    }
+
+    fn render_pixel(&self, renderer: &mut impl Renderer) {
+        let pixel = if self.control.background_and_window_enabled() {
+            self.background_pixel(self.current_position)
+        } else {
+            0b00
+        };
+        renderer.render(self.current_position, pixel)
+    }
+
     fn advance_cycle(&mut self) {
         self.cycles_in_line += 1;
         if self.cycles_in_line >= CYCLES_PER_LINE {
-            self.current_y += 1;
-            self.current_x = 0;
+            self.current_position.y += 1;
+            self.current_position.x = 0;
             self.cycles_in_line = 0;
-            if self.current_y >= LINES_PER_FRAME {
-                self.current_y = 0;
+            if self.current_position.y >= LINES_PER_FRAME {
+                self.current_position.y = 0;
             }
         }
     }
@@ -88,7 +109,7 @@ impl Ppu {
         if previous_mode != self.mode() && self.mode() == VBlank {
             interrupt_controller.request(Interrupt::VBlank);
         }
-        if self.current_y == self.y_compare && self.interrupt_source.ly()
+        if self.current_position.y == self.y_compare && self.interrupt_source.ly()
             || (previous_mode != self.mode()
                 && match self.mode() {
                     HBlank => self.interrupt_source.hblank(),
@@ -106,6 +127,9 @@ impl Ppu {
         interrupt_controller: &mut InterruptController,
         renderer: &mut impl Renderer,
     ) {
+        if !self.control.is_enabled() {
+            return;
+        }
         let current_mode = self.mode();
         if current_mode == Mode::Transfer {}
         self.advance_cycle();
@@ -127,13 +151,13 @@ impl Ppu {
     pub fn set_control_bits(&mut self, value: u8) {
         self.control.set_bits(value);
         if !self.control.is_enabled() {
-            self.current_y = 0;
+            self.current_position.y = 0;
         }
     }
 
     pub fn status_bits(&self) -> u8 {
         self.interrupt_source.bits() << 3
-            | ((self.current_y == self.y_compare) as u8) << 2
+            | ((self.current_position.y == self.y_compare) as u8) << 2
             | (self.mode() as u8)
     }
 
@@ -142,7 +166,7 @@ impl Ppu {
     }
 
     pub fn current_y(&self) -> u8 {
-        self.current_y
+        self.current_position.y
     }
 
     pub fn y_compare(&self) -> u8 {
--- core/src/ppu/vram.rs
+++ core/src/ppu/vram.rs
@@ -1,7 +1,44 @@
+use super::Coordinate;
+use std::ops::RangeInclusive;
+
+const TILE_DATA_SIZE: usize = 16;
+
+const TILE_MAP_SIZE: usize = 32;
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct TileData<'vram> {
+    data: &'vram [u8],
+}
+
+impl<'vram> TileData<'vram> {
+    fn new(data: &'vram [u8]) -> Self {
+        Self { data }
+    }
+
+    pub fn pixel(&self, position: Coordinate) -> u8 {
+        let index = position.y as usize * 2 % 8;
+        [1, 0]
+            .iter()
+            .map(|offset| self.data[index + offset])
+            .map(|byte| byte >> (7 - position.x as usize % 8))
+            .fold(0b00, |acc, bit| acc << 0 | bit)
+    }
+}
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum TileDataArea {
     Shifted,
     Origin,
+}
+
+impl TileDataArea {
+    fn address(&self, id: u8) -> usize {
+        use TileDataArea::*;
+        match self {
+            Shifted => 0x0800 + id.wrapping_sub(128) as usize,
+            Origin => id as usize * TILE_DATA_SIZE,
+        }
+    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
@@ -10,12 +47,22 @@ pub enum TileMapArea {
     Second,
 }
 
+impl TileMapArea {
+    fn range(&self) -> RangeInclusive<usize> {
+        use TileMapArea::*;
+        match self {
+            First => 0x1800..=0x1BFF,
+            Second => 0x1C00..=0x1FFF,
+        }
+    }
+}
+
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub struct Vram {
+struct VramData {
     data: Vec<u8>,
 }
 
-impl Default for Vram {
+impl Default for VramData {
     fn default() -> Self {
         Self {
             data: vec![0x00u8; 0x2000],
@@ -23,16 +70,62 @@ impl Default for Vram {
     }
 }
 
+impl VramData {
+    fn tile_map_data(&self, area: TileMapArea) -> &[u8] {
+        &self.data[area.range()]
+    }
+
+    fn tile_data(&self, area: TileDataArea, id: u8) -> TileData {
+        let address = area.address(id);
+        TileData::new(&self.data[address..(address + TILE_DATA_SIZE)])
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct TileMap<'vram> {
+    data: &'vram VramData,
+    map_area: TileMapArea,
+    data_area: TileDataArea,
+}
+
+impl<'vram> TileMap<'vram> {
+    pub fn tile_data(&self, position: Coordinate) -> TileData {
+        let id = self.data.tile_map_data(self.map_area)
+            [position.y as usize * TILE_MAP_SIZE + position.x as usize];
+        self.data.tile_data(self.data_area, id)
+    }
+
+    pub fn pixel(&self, position: Coordinate) -> u8 {
+        self.tile_data(Coordinate {
+            x: position.x / 8,
+            y: position.y / 8,
+        })
+        .pixel(position)
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Default)]
+pub struct Vram {
+    data: VramData,
+}
+
 impl Vram {
+    pub fn tile_map(&self, map_area: TileMapArea, data_area: TileDataArea) -> TileMap {
+        TileMap {
+            data: &self.data,
+            map_area,
+            data_area,
+        }
+    }
     pub fn read(&self, address: u16) -> u8 {
-        *self.data.get(address as usize).unwrap_or_else(|| {
+        *self.data.data.get(address as usize).unwrap_or_else(|| {
             log::warn!("VRAM: Attempt to read from out of bounds: {:04X}", address);
             &0x00
         })
     }
 
     pub fn write(&mut self, address: u16, value: u8) {
-        match self.data.get_mut(address as usize) {
+        match self.data.data.get_mut(address as usize) {
             Some(e) => *e = value,
             None => log::warn!("VRAM: Attempt to write to out of bounds: {:04X}", address),
         }
